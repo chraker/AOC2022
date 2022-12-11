cf0588b07d73b5dd843022b82a0e4d566ed7447f
Christopher <christopher.akersten@skf.com>
2022-12-11 14:02:15 +0100

    refactored

--- day9.py
+++ day9.py
@@ -1,18 +1,16 @@
-import time
-
-import matplotlib
 import numpy as np
 from matplotlib import pyplot as plt
-from matplotlib.pyplot import waitforbuttonpress
 
 
 def part1():
     visited = get_visited_locations(2)
+    plot_visited(visited)
     return len(get_unique_locations(visited))
 
 
 def part2():
     visited = get_visited_locations(10)
+    plot_visited(visited)
     return len(get_unique_locations(visited))
 
 
@@ -28,15 +26,7 @@ def get_visited_locations(rope_length, simulate=False):
     movements = output.split('\n')
 
     if simulate:
-        fig, ax = plt.subplots()
-        plt.ion()
-        l_body, = ax.plot(0, 0)
-        l_head, = ax.plot(0, 0)
-        l_tail, = ax.plot(0, 0)
-        l_tail_history, = ax.plot(0, 0)
-        ax.set_xlim([-25, 25])
-        ax.set_ylim([-25, 25])
-        plt.draw()
+        l_body, l_head, l_tail, l_tail_history = prepare_plot()
 
     for movement in movements:
         direction, length_str = movement.split(' ')
@@ -45,32 +35,11 @@ def get_visited_locations(rope_length, simulate=False):
             rope[0] = move(rope[0], direction)
             for index, knot in enumerate(rope):
                 if index != 0:
-                    rope[index] = follow(rope[index - 1], knot, direction)
+                    rope[index] = follow(rope[index - 1], knot)
             history.append(tuple(rope[len(rope) - 1]))
 
             if simulate:
-                x = np.array(list(map(lambda c: c[0], history)))
-                y = np.array(list(map(lambda c: c[1], history)))
-                l_tail_history.set_data(x, y)
-
-                x = np.array(list(map(lambda c: c[0], rope)))
-                y = np.array(list(map(lambda c: c[1], rope)))
-                l_body.set_data(x, y)
-
-                x = np.array(list(map(lambda c: c[0], rope[0:2])))
-                y = np.array(list(map(lambda c: c[1], rope[0:2])))
-                l_head.set_data(x, y)
-
-                x = np.array(list(map(lambda c: c[0], rope[8:9])))
-                y = np.array(list(map(lambda c: c[1], rope[8:9])))
-                l_tail.set_data(x, y)
-
-                plt.draw()
-                plt.pause(0.1)
-
-    if simulate:
-        plt.show()
-        plt.pause(100)
+                update_plot(history, l_body, l_head, l_tail, l_tail_history, rope)
     return history
 
 
@@ -88,7 +57,7 @@ def move(knot, direction):
         return [knot[0] - 1, knot[1]]
 
 
-def follow(head, tail, direction):
+def follow(head, tail):
     if head is tail:
         return tail
     is_right = head[0] - tail[0] > 1
@@ -130,3 +99,38 @@ def shares_row_or_column(head, tail):
 
 def adjacent(T, H):
     return abs(T[0] - H[0]) <= 1 and abs(T[1] - H[1]) <= 1
+
+
+def update_plot(history, l_body, l_head, l_tail, l_tail_history, rope):
+    x = np.array(list(map(lambda c: c[0], history)))
+    y = np.array(list(map(lambda c: c[1], history)))
+    l_tail_history.set_data(x, y)
+    x = np.array(list(map(lambda c: c[0], rope)))
+    y = np.array(list(map(lambda c: c[1], rope)))
+    l_body.set_data(x, y)
+    x = np.array(list(map(lambda c: c[0], rope[0:2])))
+    y = np.array(list(map(lambda c: c[1], rope[0:2])))
+    l_head.set_data(x, y)
+    x = np.array(list(map(lambda c: c[0], rope[8:9])))
+    y = np.array(list(map(lambda c: c[1], rope[8:9])))
+    l_tail.set_data(x, y)
+    plt.draw()
+    plt.pause(0.1)
+    plt.waitforbuttonpress()
+
+
+def prepare_plot():
+    fig, ax = plt.subplots()
+    plt.ion()
+    l_body, = ax.plot(0, 0)
+    l_head, = ax.plot(0, 0)
+    l_tail, = ax.plot(0, 0)
+    l_tail_history, = ax.plot(0, 0)
+    ax.set_xlim([-25, 25])
+    ax.set_ylim([-25, 25])
+    return l_body, l_head, l_tail, l_tail_history
+
+
+def plot_visited(visited):
+    plt.plot(*zip(*visited))
+    plt.show()
